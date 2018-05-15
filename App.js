95e7374a3de58768bbaff01fd55ffcb0e0ac9dd8
Chase Fisher <linkchase@gmail.com>
2018-05-15 11:08:44 -0400

    Remove Test

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\nCmd+D or shake for dev menu",
   android:
     "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu. Test"
+    "Shake or press menu button for dev menu."
 });
 
 const App = () => (
